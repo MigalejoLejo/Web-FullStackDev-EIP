864478f6cf25cea878302a7f49660f9d6194eb84
Miguel <migalejo85@gmail.com>
2024-03-31 23:39:23 +0200

    initial commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+
+
+    public function home () {
+        return 'hola mundo';
+    }
+    //
+}
