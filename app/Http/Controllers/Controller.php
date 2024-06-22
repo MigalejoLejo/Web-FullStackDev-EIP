d162605619fa1db361f56aa08d761c0bed519727
Miguel <migalejo85@gmail.com>
2024-06-22 19:58:11 +0200

    added laravel login

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController {
-
-    public function index() {
-        return view('welcome');
-    }
+class Controller extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
 }
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,12 @@
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistrationController;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', [Controller::class, 'index']);
 
-Route::get('/register', [RegistrationController::class, 'register'])->name('register');
+// Route::get('/register', [RegistrationController::class, 'register'])->name('register');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
